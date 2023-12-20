3fa002f647abf32abc215c8b7a219e00c73e6ea4
kiharito <kihara13611.c7d7@gmail.com>
2023-12-20 22:47:36 +0900

    db接続処理追加

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,20 @@
 use axum::{routing::get, Router};
+use axum_error::Result;
+use sqlx::sqlite::SqlitePool;
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(index));
+async fn main() -> Result<()> {
+    let _ = dotenv::dotenv();
+    let url = std::env::var("DATABASE_URL")?;
+    let pool = SqlitePool::connect(&url).await?;
+
+    let app = Router::new()
+        .route("/", get(index))
+        .with_state(pool)
+        .layer(CorsLayer::very_permissive());
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
 
 async fn index() -> String {
