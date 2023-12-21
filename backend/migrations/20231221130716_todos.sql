6e84227353d01e377fcd4f2b901b6be86fad4ed7
kiharito <kihara13611.c7d7@gmail.com>
2023-12-21 22:51:46 +0900

    todo一覧取得(コンパイル不可)

---
+++ backend/migrations/20231221130716_todos.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS todos
+(
+    id
+    INTEGER
+    PRIMARY
+    KEY
+    NOT
+    NULL,
+    description
+    TEXT
+    NOT
+    NULL,
+    done
+    BOOLEAN
+    NOT
+    NULL
+    DEFAULT
+    0
+)
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,7 @@
-use axum::{routing::get, Router};
+use axum::extract::State;
+use axum::{routing::get, Json, Router, debug_handler};
 use axum_error::Result;
+use serde::{Deserialize, Serialize};
 use sqlx::sqlite::SqlitePool;
 use tower_http::cors::CorsLayer;
 
@@ -10,13 +12,24 @@ async fn main() -> Result<()> {
     let pool = SqlitePool::connect(&url).await?;
 
     let app = Router::new()
-        .route("/", get(index))
+        .route("/", get(list))
         .with_state(pool)
         .layer(CorsLayer::very_permissive());
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     Ok(axum::serve(listener, app).await?)
 }
 
-async fn index() -> String {
-    format!("Hello, world!")
+#[derive(Serialize, Deserialize)]
+struct Todo {
+    id: i64,
+    description: String,
+    done: bool,
+}
+
+#[debug_handler]
+async fn list(State(pool): State<SqlitePool>) -> Result<Json<Vec<Todo>>> {
+    let todos = sqlx::query_as!(Todo, "SELECT id, description, done FROM todos ORDER BY id")
+        .fetch_all(&pool)
+        .await?;
+    Ok(Json(todos))
 }
